0a2d34676e18f51ac7b4487c9c669a97b3f4de3c
Tarzesher <Tarzesher@gmail.com>
2017-02-06 13:29:50 +0200

    TpyeScript Beginner

---
+++ TypeScriptPractice/app.js
@@ -0,0 +1,10 @@
+var b = {
+    title: "Blood in the Sand",
+    author: "Tedesca",
+    bookInfo: function () {
+        return this.title + " by :" + this.author;
+    }
+};
+var book = b.bookInfo();
+alert(book);
+//# sourceMappingURL=app.js.map
---
+++ TypeScriptPractice/app.ts
@@ -0,0 +1,16 @@
+﻿interface IBook {
+    title: string;
+    author: string;
+    bookInfo: () => string;
+}
+
+var b: IBook = {
+    title: "Blood in the Sand",
+    author: "Tedesca",
+      bookInfo: function() {
+          return this.title + " by :" + this.author;
+      }
+}
+
+var book = b.bookInfo();
+alert(book);
